ccb5792a46cb91bcfc811d43e430cbcb228f2984
ssumika <gsuzuki221@gmail.com>
2022-10-18 17:54:02 +0900

    [5.7]変数の埋め込み

--- tutorial/dev10/sample2/routes/index.js
+++ tutorial/dev10/sample2/routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function(request, response) {
 router.get('/rakus', function(request, response) {
     // views/rakus.hbs を表示する
     // 第2引数で views/rakus.hbs の {{title}}, {{message}} に表示する文字をセットしている
-    response.render('rakus', {/* ここに表示したい値を記述する */});
+    response.render('rakus', {title:'こんにちは！',message:'Rakus'});
 });
 
 // ルーティングオブジェクトを外部ファイルへエクスポートする
