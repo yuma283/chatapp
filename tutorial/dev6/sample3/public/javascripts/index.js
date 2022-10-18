d1fd17368dc2dc6411d4532b18792469565d6ac9
izumitomo <luckystar.tomosuke@gmail.com>
2022-10-18 17:50:24 +0900

    リアルタイム通信の実装完了

--- tutorial/dev6/sample3/public/javascripts/index.js
+++ tutorial/dev6/sample3/public/javascripts/index.js
@@ -19,11 +19,11 @@ function sendMessage() {
                     'このメッセージはすべてのクライアントに送信されます。');
 
     // メッセージ入力イベント（sendMessageEvent）を送信する
-
+    socket.emit('sendMessageEvent', message);
 }
 
 // メッセージ表示イベント（receiveMessageEvent）を受信する
-socket.on('', function (data) {
+socket.on('receiveMessageEvent', function (data) {
     // 画面上にメッセージを表示
-    $('#thread').prepend('<p>' + '</p>');
+    $('#thread').prepend('<p>' + data + '</p>');
 });
