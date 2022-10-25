e8963193aa5f5255995d01c33eda984785ccdc35
izumitomo <luckystar.tomosuke@gmail.com>
2022-10-25 18:40:24 +0900

    ユーザ名の追加

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,10 +3,10 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = $('#message').val();;
-    // メモの内容を表示
+    const input_message = $('#message').val();;
+    const message = userName + "さんのメモ：" + input_message    // メモの内容を表示
     $('#thread').prepend('<p>' + message + '</p>'); 
     return false;
 }
