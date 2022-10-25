f3d069eb0c660da40b07a3572bfd917c56f4bacf
ssumika <gsuzuki221@gmail.com>
2022-10-25 18:02:31 +0900

    enter.js変更

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,12 +1,20 @@
 'use strict';
 
 // 入室メッセージをサーバに送信する
+
 // 入力されたユーザ名を取得する
-const userName = '';
+const userName = $('#userName').val();
 // 入室メッセージイベントを送信する
-
-
+socket.emit('enterMyselfEvent',userName);
+socket.emit('enterOtherEvent',userName);
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('enterMyselfEvent', function (data) {
+    // 画面上にメッセージを表示
+    $('#thread').prepend('<p>' + data + '</p>');
+});
+
+// 自クライアント以外の接続イベント（enterOtherEvent）を受信する
+socket.on('enterOtherEvent', function (data) {
+    // 画面上にメッセージを表示
+    $('#thread').prepend('<p>' + data + '</p>');
 });
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,11 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
+    socket.on('enterMyselfEvent', function (data) {
+        // 自クライアントに接続イベント（enteringMyselfEvent）を送信する
+        socket.emit('enterMyselfEvent', 'あなたが接続しました。');
 
+        // 自クライアント以外に接続イベント（enterOtherEvent）を送信する
+        socket.broadcast.emit('enterMyselfEvent', data+'他のクライアントが接続しました。');
     });
 };
