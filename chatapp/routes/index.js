52430cac744c0b1ca300959dc352a31a26effa14
yuma283 <twopiece0324@gmail.com>
2022-10-25 17:45:54 +0900

    ユーザ名埋め込み

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -11,9 +11,8 @@ router.get('/', function(request, response, next) {
 
 // チャット画面の表示
 router.post('/room', function(request, response, next) {
-  const userName = $('#userName').val();
     console.log('ユーザ名：' + request.body.userName);
-    response.render('room', { myname: userName });
+    response.render('room', { userName: request.body.userName });
 });
 
 module.exports = router;
