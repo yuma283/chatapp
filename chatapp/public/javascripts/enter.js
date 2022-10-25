d2ea0c050e3433793458a4aa180f16e8894b112f
ssumika <gsuzuki221@gmail.com>
2022-10-25 18:25:04 +0900

    入室処理完了(userNameは未対応)

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -4,9 +4,10 @@
 
 // 入力されたユーザ名を取得する
 const userName = $('#userName').val();
+console.log(userName);
 // 入室メッセージイベントを送信する
-socket.emit('enterMyselfEvent',userName);
-socket.emit('enterOtherEvent',userName);
+socket.emit('enterMyselfEvent','あなたが入室しました');
+socket.emit('enterOtherEvent',userName+'さんが入室しました');
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('enterMyselfEvent', function (data) {
     // 画面上にメッセージを表示
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -4,9 +4,10 @@ module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
     socket.on('enterMyselfEvent', function (data) {
         // 自クライアントに接続イベント（enteringMyselfEvent）を送信する
-        socket.emit('enterMyselfEvent', 'あなたが接続しました。');
-
+        socket.emit('enterMyselfEvent', data);
+    });
+    socket.on('enterOtherEvent', function (data) {
         // 自クライアント以外に接続イベント（enterOtherEvent）を送信する
-        socket.broadcast.emit('enterMyselfEvent', data+'他のクライアントが接続しました。');
+        socket.broadcast.emit('enterOtherEvent', data);
     });
 };
