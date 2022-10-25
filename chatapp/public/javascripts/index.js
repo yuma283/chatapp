d43d745c3b9c0c287b3a74ab9b5a3cba444d1a19
yuma283 <twopiece0324@gmail.com>
2022-10-25 17:06:33 +0900

    ユーザ名埋め込み1

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -4,13 +4,13 @@
 function enter() {
     // 入力されたユーザ名を取得する
     const userName = $('#userName').val();
+    // ユーザ名が未入力でないかチェックする
+
+
+    $('form').submit();
     router.get('/room', function(request, response) {
       // views/rakus.hbs を表示する
       // 第2引数で views/rakus.hbs の {{title}}, {{message}} に表示する文字をセットしている
       response.render('room', {login_user:userName/* ここに表示したい値を記述する */});
     });
-    // ユーザ名が未入力でないかチェックする
-
-
-    $('form').submit();
 }
