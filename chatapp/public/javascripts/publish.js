60e80b16b68f24bc6308fe3ac63a45be0abcc023
yuki-712 <arsenal0712@akane.waseda.jp>
2022-11-01 18:01:36 +0900

    コンフリクト解消

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -26,9 +26,8 @@ function publish() {
     var formatNowDate = YYYY + "/" + MM + "/" + DD + " " + HH + ":" + mm;
     // 時刻をメッセージに追加
     const message = userName + "さん：" + input_message + "（" + formatNowDate + "）";
-
     // 最後にメッセージを送信したユーザーを変えるイベントの送信
-    socket.emit('sendchangeUserEvent',userName);
+    socekt.emit('sendchangeUserEvent', userName);
     // 投稿内容を送信
     // メッセージ入力イベント（sendMessageEvent）を送信する
     socket.emit('sendMessageEvent', message);
